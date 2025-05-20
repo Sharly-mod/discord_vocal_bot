cf98f3033e43d1cf21343f7062ab00a23f0d8c5b
sharl <sharlyfarag@gmail.com>
2025-05-20 11:46:20 +0200

    update 1.4.3

--- bot.py
+++ bot.py
@@ -62,12 +62,12 @@ class InviteSelect(ui.Select):
 
         options = [
             discord.SelectOption(label=member.display_name, value=str(member.id))
-            for member in members if not member.bot and member != author][:25]  # MAX 25 membres
+            for member in members if not member.bot and member != author][:200]  # MAX 25 membres
 
         super().__init__(
             placeholder="Choisis un membre à inviter",
             min_values=1,
-            max_values > 1,
+            max_values = 200,
             options=options
         )
 
